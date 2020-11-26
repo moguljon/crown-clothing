7d455152970bfd0012ecc2037cf318b391a89586
unknown <jonathanaguilar@moguljon.com>
2020-11-26 18:08:39 -0500

    added some routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Homepage from './pages/homepage/homepage';
+
+const HatsPage = (props) => {
+  console.log(props);
+  return(
+    <div>HatsPage</div>
+  )
+}
 
 function App() {
   return (
     <div>
-      <Homepage />
-  
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
+        
     </div>
   );
 }
